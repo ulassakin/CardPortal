947fba3c033dcdc10370fae849d9939910942fa1
ulassakin <mulassakin@gmail.com>
2025-09-08 12:42:58 +0300

    menu file updated

--- src/menu.cpp
+++ src/menu.cpp
@@ -19,12 +19,15 @@ String SSID = "";
 String pass = "";
 int cursorPosition = 0;
 
+String currentText = "";
+
 bool cursorVisible = true;
 unsigned long lastBlink = 0;
 const unsigned long blinkInterval = 500; // ms
 
 M5GFX &dsp = M5Cardputer.Display;
 bool pressDrawn = false;
+
 AppState currentState = PRESS;
 
 M5Canvas portalCanvas(&M5Cardputer.Display);
@@ -47,6 +50,50 @@ void drawPress() {
     dsp.setCursor(20,90);
     dsp.print("Press To Start");
     
+}
+
+void drawWithCursor() {
+    portalCanvas.fillScreen(BLACK);
+    portalCanvas.setFont(&PressStart2P_Regular7pt7b);
+    
+
+    const int startX = 0;
+    const int startY = 0;
+
+    String header = (currentState == ENTER_SSID) 
+                  ? "Enter SSID:"
+                  : "Enter Password: ";
+
+    portalCanvas.setCursor(startX, startY);
+    portalCanvas.setTextColor(WHITE, BLACK);
+    portalCanvas.print(header);
+    portalCanvas.print(currentText);
+
+    // Monospace cursor calculation
+    int charWidth = portalCanvas.textWidth("A");
+    int fontHeight = portalCanvas.fontHeight();
+    int lineWidth = portalCanvas.width();
+    int charsPerLine = lineWidth / charWidth;
+
+    int totalChars = header.length() + cursorPosition;
+    int lineNumber = totalChars / charsPerLine;
+    int lineOffset = totalChars % charsPerLine;
+
+    int cursorX;
+    int cursorY;
+
+    if (lineNumber >= 8 ){
+        cursorX = lineOffset * charWidth;
+        cursorY = 7 * fontHeight + 5;
+    } else {
+        cursorX = startX + lineOffset * charWidth;
+        cursorY = startY + lineNumber * fontHeight;
+    }
+
+    if (cursorVisible) {
+        portalCanvas.drawLine(cursorX, cursorY, cursorX, cursorY + fontHeight, WHITE);
+    }
+    portalCanvas.pushSprite(0, 0);
 }
 
 void menu_loop() {
