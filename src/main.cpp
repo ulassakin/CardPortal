176b8211299b7cb1228a2e8e5e8c30b6e5448239
ulassakin <mulassakin@gmail.com>
2025-09-08 13:14:00 +0300

    Main file updated

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,25 @@
 #include <M5Cardputer.h>
 #include "intro.h"
 #include "evil_portal.h"
+#include "menu.h"
+
+
 
 void setup() {
   auto cfg = M5.config();
   M5Cardputer.begin(cfg);
+  menu_setup();
+
   runMinimalIntro();   // Intro
-  portal_begin();      // Start the captive portal
+  // portal_begin();      // Start the captive portal
+  currentState = PRESS;
+  
 }
 
 void loop() {
-  portal_loop_once();  // Captive Portal loop
+  if (currentState == RUNNING) {
+    portal_loop_once();   // Captive portal aktif
+  } else {
+    menu_loop();          // Menü state machine
+  } 
 }
