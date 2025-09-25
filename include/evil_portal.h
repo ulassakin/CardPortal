fb278208e5530a5d98ccbfe9d0a5ef4aa4c1fbad
ulassakin <mulassakin@gmail.com>
2025-09-25 14:20:38 +0300

    setter functions added

--- include/evil_portal.h
+++ include/evil_portal.h
@@ -5,3 +5,6 @@ void portal_begin();
 
 // Portal döngüsü (eski loop fonksiyonunun içeriği)
 void portal_loop_once();
+void portal_set_ssid(const char* ssid);
+void portal_set_pass(const char* pass);        // pass == NULL -> şifresiz AP
+void portal_set_html(const String& html);
